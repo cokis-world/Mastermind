5b86c5afeffcc8d52cb482e0b53accd283d39a0f
Gizela <gizela@webagentur.at>
2020-05-26 15:27:02 +0200

    #2 generate random numbers

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
 echo 'Test';
+
+$numbers = generateNumbers();
+var_dump($numbers);
+
+/**
+ * Generates random numbers; each between 0 - 9
+ * @return string
+ */
+function generateNumbers()
+{
+    $numbers = '';
+
+    for ($i = 0; $i < 4; $i++) {
+        $numbers .= rand(0, 9) . ' ';
+    }
+
+    return $numbers;
+}
